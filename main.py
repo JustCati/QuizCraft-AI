c966a6541b1114654939ce098bc9314b25926ba0
JustCati <ale.catinello@gmail.com>
2024-09-08 16:28:59 +0200

    revert: drop support for unstrucutred

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 import os
 import os.path as osp
-from src.utils.docker import manage_container
+
+from src.unstructured.extract import extract
 
 
-
-@manage_container
 def main(*args, **kwargs):
-    ...
+    data_path = osp.join(os.getcwd(), "data")
+    raw_data_path = osp.join(data_path, "RAW")
+    processed_data_path = osp.join(data_path, "EXTRACTED")
+
+    for rdir in sorted(os.listdir(raw_data_path)):
+        if rdir.startswith("."):
+            continue
+        for dir in sorted(os.listdir(osp.join(raw_data_path, rdir))):
+            if dir.startswith("."):
+                continue
+            if dir != "Question":
+                continue
+            print(f"Processing {rdir, dir}...")
+            for file in sorted(os.listdir(osp.join(raw_data_path, rdir, dir))):
+                if file.startswith("."):
+                    continue
+                if file.endswith(".jpg"):
+                    file_path = osp.join(osp.basename(osp.dirname(raw_data_path)), osp.basename(raw_data_path), rdir, dir, file)
+
+                    extracted_data = extract(file_path, slide = dir == "Slide")
+                    
+
+                    print(f"Extracted data: {extracted_data}")
+                    return
+
 
 
 
 if __name__ == '__main__':
-    unstructured_container_name = "unstructured"
-    unstructured_container_path = osp.join(os.getcwd(), "src", "unstructured")
-
-    main(path = unstructured_container_path, container = unstructured_container_name)
+    main()
