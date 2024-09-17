40974a1db94e425507743d9d0470c31f99c424f5
JustCati <ale.catinello@gmail.com>
2024-09-17 13:56:46 +0200

    chore: drop docker

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from src.text.extract import extract
 from src.model.model import getModelAndProcessor
 
 
-def main(*args, **kwargs):
+def main():
     data_path = osp.join(os.getcwd(), "data")
     raw_data_path = osp.join(data_path, "RAW")
-    processed_data_path = osp.join(data_path, "EXTRACTED")
 
 
     PROMPT = ""
@@ -17,27 +16,24 @@ def main(*args, **kwargs):
     with open(osp.join("src", "model", "prompt.txt"), "r") as f:
         PROMPT = f.read()
         print(f"Prompt: {PROMPT}")
+        print()
 
 
     for rdir in sorted(os.listdir(raw_data_path)):
         if rdir.startswith("."):
             continue
         for dir in sorted(os.listdir(osp.join(raw_data_path, rdir))):
-            if dir.startswith("."):
-                continue
-            if dir != "Slide":
+            if dir.startswith(".") or (dir != "Question" and dir != "Slide"):
                 continue
             print(f"Processing {rdir, dir}...")
             for file in sorted(os.listdir(osp.join(raw_data_path, rdir, dir))):
                 if file.startswith("."):
                     continue
-                if file.endswith(".pdf"):
+                if file.endswith(".pdf") or file.endswith(".jpg"):
                     file_path = osp.join(osp.basename(osp.dirname(raw_data_path)), osp.basename(raw_data_path), rdir, dir, file)
 
                     extracted_data = extract(file_path, model, processor, PROMPT)
-
                     print(f"Extracted data: {extracted_data}")
-                    return
 
 
 
