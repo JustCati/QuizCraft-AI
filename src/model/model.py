b59c9d4ad7c07cadebb8691605f9b88515bb844d
JustCati <ale.catinello@gmail.com>
2024-10-01 16:01:15 +0200

    feat: OllamaModel class

--- src/model/model.py
+++ src/model/model.py
@@ -1,50 +1,28 @@
 import subprocess
-from src.utils.pdf2img import convert_to_base64
-
 from langchain_ollama import ChatOllama
-from langchain_core.messages import HumanMessage
-from langchain_core.output_parsers import StrOutputParser
 
 
+# # "llava:34b-v1.6-q3_K_M"
+class OllamaModel:
+    def __init__(self, model_name, temperature = 0.0, num_predict = 512):
+        self.model_name = model_name
+        self.temperature = temperature
+        self.num_predict = num_predict
+
+        self.model = ChatOllama(model=self.model_name, 
+                          temperature=self.temperature,
+                          num_predict=self.num_predict)
 
 
-def manage_Model(name, action = "run"):
-    if action not in ["run", "stop"]:
-        raise ValueError(f"Invalid action: {action}")
-    subprocess.Popen(['ollama', action, name], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    def __manage_Model(self, action = "run"):
+        if action not in ["run", "stop"]:
+            raise ValueError(f"Invalid action: {action}")
+        subprocess.Popen(['ollama', action, self.model_name], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
 
+    def __enter__(self):
+        self.__manage_Model("run")
+        return self.model
 
-def prompt_func(data):
-    text = data["text"]
-    image = data["image"]
-
-    image_part = {
-        "type": "image_url",
-        "image_url": f"data:image/jpeg;base64,{image}",
-    }
-
-    content_parts = []
-
-    text_part = {"type": "text", "text": text}
-
-    content_parts.append(image_part)
-    content_parts.append(text_part)
-
-    return [HumanMessage(content=content_parts)]
-
-
-# "llava:34b-v1.6-q3_K_M"
-def getModel(name, temperature = 0.0, num_predict = 512):
-    manage_Model(name, "run")
-    return ChatOllama(model=name, 
-                       temperature=temperature,
-                       num_predict=num_predict)
-
-
-def extract_text_from_image(model, img, prompt):
-    img = convert_to_base64(img)
-
-    chain = prompt_func | model | StrOutputParser()
-    query_chain = chain.invoke({"text": prompt, "image": img})
-    return query_chain
+    def __exit__(self):
+        self.__manage_Model("stop")
