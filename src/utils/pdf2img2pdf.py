e70fb320975f35f648679592460f44853103c507
JustCati <ale.catinello@gmail.com>
2024-10-04 17:32:49 +0200

    feat: img to pdf converted

--- src/utils/pdf2img2pdf.py
+++ src/utils/pdf2img2pdf.py
@@ -1,16 +1,26 @@
 import base64
 import tempfile
+from PIL import Image
 from io import BytesIO
 
 from pdf2image import convert_from_path
 
 
 
-def convert_to_img(pdf_file_path):
+def convert_pdf2img(pdf_file_path):
     with tempfile.TemporaryDirectory() as dir:
         images_from_path = convert_from_path(pdf_file_path, output_folder=dir)
         print(f"Extracted {len(images_from_path)} images from {pdf_file_path}")
         return images_from_path
+
+
+def convert_img2pdf(path, output_file_path):
+    for img in path:
+        image = Image.open(img)
+        image.save(output_file_path, "PDF", resolution=100.0, save_all=True, append_images=[image])
+    print(f"Saved images as PDF to {output_file_path}")
+    return output_file_path
+
 
 
 def convert_to_base64(img):
