dc8a2dd15a61d4756f99e34f931ba92111d843a4
JustCati <ale.catinello@gmail.com>
2024-10-10 09:24:06 +0200

    feat: model downloading when missing

--- src/model/model.py
+++ src/model/model.py
@@ -16,8 +16,10 @@ class GenericOllamaModel(object):
 
     def check_model(self):
         if self.model_name not in self.available_models:
-            raise ValueError(f"Model {self.model_name} not available. Available models are: {self.available_models}")
-        return True
+            print(f"Model {self.model_name} not available.")
+            print("Retrieving model...")
+            process = subprocess.Popen(["ollama", "pull", self.model_name], stdout=subprocess.PIPE, bufsize=0)
+            process.wait()
 
     def __manage_Model(self, action = "run"):
         if action not in ["run", "stop"]:
@@ -36,15 +38,15 @@ class GenericOllamaModel(object):
 class LanguageModel(GenericOllamaModel):
     def __init__(self, model_name, temperature = 0.0, num_predict = 512):
         super().__init__(model_name)
-        if super().check_model():
-            self.model = ChatOllama(model=model_name, 
-                            temperature=temperature,
-                            num_predict=num_predict)
+        super().check_model()
+        self.model = ChatOllama(model=model_name, 
+                        temperature=temperature,
+                        num_predict=num_predict)
 
 
 
 class EmbeddingModel(GenericOllamaModel):
     def __init__(self, model_name):
         super().__init__(model_name)
-        if super().check_model():
-            self.model = OllamaEmbeddings(model=model_name)
+        super().check_model()
+        self.model = OllamaEmbeddings(model=model_name)
