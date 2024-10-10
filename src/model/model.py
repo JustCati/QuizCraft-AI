fed0eba02780e48bcaae0339cf717d206d94c923
JustCati <ale.catinello@gmail.com>
2024-10-10 09:13:52 +0200

    feat: check model existance

--- src/model/model.py
+++ src/model/model.py
@@ -1,24 +1,28 @@
 import subprocess
 from langchain_ollama import ChatOllama, OllamaEmbeddings
-from langchain_core.language_models.chat_models import BaseChatModel
-
-
-# qwen2.5:32b-instruct-q3_K_M
-# mxbai-embed-large
 
 
 
-class GenericOllamaModel:
-    def __init__(self, model_name, temperature = 0.0, num_predict = 512):
+
+class GenericOllamaModel(object):
+    def __init__(self, model_name):
         self.model_name = model_name
-        self.temperature = temperature
-        self.num_predict = num_predict
+        self.available_models = self.__retrieve_available_models()
+
+    def __retrieve_available_models(self):
+        process = subprocess.run(["ollama", "list"], stdout=subprocess.PIPE)
+        outFolder = process.stdout.decode("utf-8").split("\n")[1:-1]
+        return [x.split(" ")[0] for x in outFolder]
+
+    def check_model(self):
+        if self.model_name not in self.available_models:
+            raise ValueError(f"Model {self.model_name} not available. Available models are: {self.available_models}")
+        return True
 
     def __manage_Model(self, action = "run"):
         if action not in ["run", "stop"]:
             raise ValueError(f"Invalid action: {action}")
         subprocess.Popen(['ollama', action, self.model_name], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-
 
     def __enter__(self):
         self.__manage_Model("run")
@@ -31,13 +35,16 @@ class GenericOllamaModel:
 
 class LanguageModel(GenericOllamaModel):
     def __init__(self, model_name, temperature = 0.0, num_predict = 512):
-        self.model = ChatOllama(model=model_name, 
-                          temperature=temperature,
-                          num_predict=num_predict)
-        super().__init__(model_name, temperature, num_predict)
+        super().__init__(model_name)
+        if super().check_model():
+            self.model = ChatOllama(model=model_name, 
+                            temperature=temperature,
+                            num_predict=num_predict)
+
 
 
 class EmbeddingModel(GenericOllamaModel):
     def __init__(self, model_name):
-        self.model = OllamaEmbeddings(model=model_name)
         super().__init__(model_name)
+        if super().check_model():
+            self.model = OllamaEmbeddings(model=model_name)
