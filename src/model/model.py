47e1f20ab7215f6a6faab6349b40e1ce6393f158
JustCati <ale.catinello@gmail.com>
2024-11-26 15:57:47 +0100

    feat: stop running models

--- src/model/model.py
+++ src/model/model.py
@@ -1,22 +1,35 @@
 import subprocess
+from langchain_ollama import ChatOllama
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_ollama import ChatOllama, OllamaEmbeddings
 
 
 
 class GenericOllamaModel(object):
     def __init__(self, model_name):
         self.model_name = model_name
-        self.available_models = self.__retrieve_available_models()
+        self.available_models = self.__retrieve_available_models("available")
 
-    def __retrieve_available_models(self):
-        process = subprocess.run(["ollama", "list"], stdout=subprocess.PIPE)
+    def __retrieve_info(self, operation):
+        process = subprocess.run(["ollama", str(operation)], stdout=subprocess.PIPE)
         outFolder = process.stdout.decode("utf-8").split("\n")[1:-1]
-        models = [x.split(" ")[0] for x in outFolder]
-        models = [x.replace(":latest", "") for x in models]
-        return models
+        data = [x.split(" ")[0] for x in outFolder]
+        data = [x.replace(":latest", "") for x in data]
+        return data
+
+    def __retrieve_available_models(self, scope="available"):
+        if scope not in ["available", "running"]:
+            raise ValueError(f"Invalid scope: {scope}")
+        if scope == "available":
+            return self.__retrieve_info("list")
+        if scope == "running":
+            return self.__retrieve_info("ps")
 
     def check_model(self):
+        running_models = self.__retrieve_available_models("running")
+        if len(running_models) > 0:
+            for model in running_models:
+                if model == self.model_name:
+                    self.__manage_Model("stop")
         if self.model_name not in self.available_models:
             print(f"Model {self.model_name} not available.")
             print("Retrieving model...")
@@ -27,6 +40,13 @@ class GenericOllamaModel(object):
         if action not in ["run", "stop"]:
             raise ValueError(f"Invalid action: {action}")
         subprocess.Popen(['ollama', action, self.model_name], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
+    def get(self):
+        self.__manage_Model("run")
+        return self.model
+
+    def stop(self):
+        self.__manage_Model("stop")
 
     def __enter__(self):
         self.__manage_Model("run")
