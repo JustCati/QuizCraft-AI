9d8799dd7a51f091da92680f41d004f0ab78f02d
JustCati <ale.catinello@gmail.com>
2024-11-26 15:58:54 +0100

    feat: settings in frontend

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-src/gui/frontend.py
+@cl.on_settings_update
+async def setup_agent(settings):
+    llm = OllamaLanguageModel(settings["Model"], settings["Temperature"]).get()
+    cl.user_session.set("llm", llm)
+
+    settings = await cl.ChatSettings(
+        [
+            Select(
+                id="Model",
+                label="Gwen2.5 Model",
+                values=["qwen2.5:7b", "qwen2.5:32b"],
+                initial_index=0,
+                initial="qwen2.5:32b",
+            ),
+            Switch(id="Streaming", label="Stream Tokens", initial=True),
+            Slider(
+                id="Temperature",
+                label="Temperature",
+                initial=0,
+                min=0,
+                max=1,
+                step=0.1,
+            ),
+        ]
+    ).send()
+
+    await setup_agent(settings)
