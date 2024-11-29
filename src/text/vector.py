d023eecb6d8caa772a7201f930259ee050dd79da
JustCati <ale.catinello@gmail.com>
2024-11-29 09:47:35 +0100

    feat: VectorStore class

---
+++ src/text/vector.py
@@ -0,0 +1,33 @@
+from langchain_chroma import Chroma
+from langchain.text_splitter import MarkdownTextSplitter
+from langchain_experimental.text_splitter import SemanticChunker
+
+
+
+
+class VectorStore():
+    def __init__(self, embed_model, chunker="semantic"):
+        self.embed_model = embed_model
+        self.vector_store = Chroma(
+                    collection_name = "documents",
+                    embedding_function = self.embed_model
+                )
+        if chunker == "semantic":
+            self.splitter = SemanticChunker(embeddings=self.embed_model, breakpoint_threshold_type="percentile")
+        elif chunker == "markdown":
+            self.splitter = MarkdownTextSplitter(embeddings=self.embed_model)
+        else:
+            raise ValueError(f"Invalid chunker: {chunker}")
+
+    def __get_semantic_doc(self, text):
+        docs = self.splitter.create_documents(text)
+        docs = self.splitter.split_documents(docs)
+        return docs
+
+    def index_files(self, texts):
+        for text in texts:
+            docs = self.__get_semantic_doc(text)
+            self.vector_store.add_documents(docs)
+
+    def get_retriever(self):
+        return self.vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 6})
